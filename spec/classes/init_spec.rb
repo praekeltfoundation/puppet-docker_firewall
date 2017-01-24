70a650e8102c4f68b4212f0ca3d45fcec17bb5f3
Jamie Hewland <jhewland@gmail.com>
2017-01-24 09:22:35 +0200

    Add some basic tests :)

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,6 +8,157 @@ describe 'docker_firewall' do
       end
 
       it { should compile }
+
+      describe 'with default options' do
+        it { is_expected.to contain_class('firewall') }
+
+        it do
+          is_expected.to contain_firewallchain('PREROUTING:nat:IPv4')
+            .with_ensure('present')
+            .with_purge(true)
+            .with_ignore([])
+            .with_policy(nil)
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '100 DOCKER table PREROUTING LOCAL traffic'
+          ).with_table('nat')
+            .with_chain('PREROUTING')
+            .with_dst_type('LOCAL')
+            .with_proto('all')
+            .with_jump('DOCKER')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('OUTPUT:nat:IPv4')
+            .with_ensure('present')
+            .with_purge(true)
+            .with_ignore([])
+            .with_policy(nil)
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '100 DOCKER chain, route LOCAL non-loopback traffic to DOCKER'
+          ).with_table('nat')
+            .with_chain('OUTPUT')
+            .with_destination('! 127.0.0.0/8')
+            .with_dst_type('LOCAL')
+            .with_proto('all')
+            .with_jump('DOCKER')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('POSTROUTING:nat:IPv4')
+            .with_ensure('present')
+            .with_purge(true)
+            .with_ignore(
+              [
+                '^-A POSTROUTING -s (?<source>(?:[0-9]{1,3}\.){3}[0-9]{1,3})\/'\
+                '32 -d (\g<source>)\/32 .* -j MASQUERADE$'
+              ]
+            ).with_policy(nil)
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '100 DOCKER chain, MASQUERADE docker bridge traffic not bound to '\
+            'docker bridge'
+          ).with_table('nat')
+            .with_chain('POSTROUTING')
+            .with_source('172.17.0.0/16')
+            .with_outiface('! docker0')
+            .with_proto('all')
+            .with_jump('MASQUERADE')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('DOCKER:nat:IPv4')
+            .with_ensure('present')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('FORWARD:filter:IPv4')
+            .with_purge(true)
+            .with_ignore([])
+            .with_policy('drop')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('DOCKER-ISOLATION:filter:IPv4')
+            .with_ensure('present')
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '100 send FORWARD traffic to DOCKER-ISOLATION chain'
+          ).with_table('filter')
+            .with_chain('FORWARD')
+            .with_proto('all')
+            .with_jump('DOCKER-ISOLATION')
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '101 accept docker0 traffic to other interfaces on FORWARD chain'
+          ).with_table('filter')
+            .with_chain('FORWARD')
+            .with_iniface('docker0')
+            .with_outiface('! docker0')
+            .with_proto('all')
+            .with_action('accept')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('DOCKER:filter:IPv4')
+            .with_ensure('present')
+        end
+
+        it do
+          is_expected.to contain_firewallchain('DOCKER_INPUT:filter:IPv4')
+            .with_ensure('present')
+            .with_purge(true)
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '102 send FORWARD traffic for docker0 to DOCKER_INPUT chain'
+          ).with_table('filter')
+            .with_chain('FORWARD')
+            .with_outiface('docker0')
+            .with_proto('all')
+            .with_jump('DOCKER_INPUT')
+        end
+
+        it do
+          is_expected.to contain_firewall('999 drop DOCKER_INPUT traffic')
+            .with_table('filter')
+            .with_chain('DOCKER_INPUT')
+            .with_proto('all')
+            .with_action('drop')
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '100 accept related, established traffic in DOCKER_INPUT chain'
+          ).with_table('filter')
+            .with_chain('DOCKER_INPUT')
+            .with_ctstate(['RELATED', 'ESTABLISHED'])
+            .with_proto('all')
+            .with_action('accept')
+        end
+
+        it do
+          is_expected.to contain_firewall(
+            '100 accept traffic from docker0 DOCKER_INPUT chain'
+          ).with_table('filter')
+            .with_chain('DOCKER_INPUT')
+            .with_iniface('docker0')
+            .with_proto('all')
+            .with_action('accept')
+        end
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,22 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+# Add facts for the docker0 interface
+def add_interface(new_interface, existing_interfaces)
+  interfaces = existing_interfaces.split(',')
+  interfaces << new_interface
+  interfaces.sort.uniq.join(',')
+end
+
+add_custom_fact(
+  :interfaces,
+  ->(_os, facts) { add_interface('docker0', facts[:interfaces]) }
+)
+{
+  :ipaddress_docker0 => '172.17.0.1',
+  :macaddress_docker0 => '02:42:41:0b:31:b8',
+  :mtu_docker0 => '1500',
+  :netmask_docker0 => '255.255.0.0',
+  :network_docker0 => '172.17.0.0'
+}.each { |k, v| add_custom_fact k, v }
